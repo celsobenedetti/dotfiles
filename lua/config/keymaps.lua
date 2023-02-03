bc33b122eee616b3ec538ae808f96b9aef321340
Celso Benedetti <celsobenedetti2@gmail.com>
2023-02-03 09:53:52 -0300

    new: go to middle of line keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,18 +3,23 @@
 -- Add any additional keymaps here
 --
 
--- -- Unset LazyVim's default bindings
--- vim.keymap.del("i", "<c-k>")
+-- -- Unset bindings
+-- vim.keymap.del("i", "<c-c>")
+Map("<C-c>", "<Nop>", { desc = "Disable default <C-c>", mode = "i" })
 
 Map("<A-F>", require("lazyvim.plugins.lsp.format").format, { desc = "Format Document" })
 Map("<A-F>", require("lazyvim.plugins.lsp.format").format, { desc = "Format Range", mode = "v" })
 Map("R", require("lazyvim.plugins.lsp.keymaps").rename, { desc = "Rename" })
 
+Map("gm", ":call cursor(0, len(getline('.'))/2)<CR>", { desc = "Go the middle of line" })
+
 -- Navigate buffers
 Map("<A-l>", ":bnext<CR>", { mode = { "n", "v" } })
 Map("<A-h>", ":bprevious<CR>", { mode = { "n", "v" } })
 
-Map("p", '"_dP', { mode = "v", desc = "Paste without losing register" }) --hold on to register when pasting and replace text
+--hold on to register when pasting and replace text
+Map("p", '"_dP', { mode = "v", desc = "Paste without losing register" })
+
 
 Map("<leader>ma", ":Mason<CR>")
 Map("<leader>dif", ":Gitsigns diffthis<CR>")

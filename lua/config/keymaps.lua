44943b1f0ce77bcb9e6d6ec4ef4d75a4d4cdf1e5
Celso Benedetti <celsobenedetti2@gmail.com>
2023-03-19 11:12:36 -0300

    new: add position to jumplist before relative jump 🙌

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,3 +26,21 @@ Map("<leader>S", ":w<CR>")
 Map("<leader>W", ":w<CR>")
 
 Map("Q", ":bdelete<CR>")
+
+-- if doing relative jump, add current position to jump list
+local function track_position_before_relative_jump(key)
+  local count = vim.v.count or 1
+  local orig_key = vim.api.nvim_replace_termcodes(key, true, true, true)
+  if count > 1 then
+    vim.api.nvim_feedkeys("m'", "n", true)
+  end
+  vim.api.nvim_feedkeys(count .. orig_key, "n", true)
+end
+
+Map("j", function()
+  track_position_before_relative_jump("j")
+end)
+
+Map("k", function()
+  track_position_before_relative_jump("k")
+end)

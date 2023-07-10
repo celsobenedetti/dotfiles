da709e0b2fb75b14fd07137f3116f24c325ae669
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-10 11:19:49 -0300

    🗑️ remove: neocompose

--- README.md
+++ README.md
@@ -4,20 +4,6 @@ A starter template for [ ](https://github.com/LazyVim/LazyVim)
 
 - ## Usages
 
-### NeoComposer
-
-| Function            | Keymap  | Action                                                                                                              |
-| ------------------- | ------- | ------------------------------------------------------------------------------------------------------------------- |
-| `play_macro`        | `Q`     | Plays queued macro                                                                                                  |
-| `stop_macro`        | `cq`    | Halts macro playback                                                                                                |
-| `toggle_macro_menu` | `<m-q>` | Toggles popup macro menu                                                                                            |
-| `cycle_next`        | `<c-n>` | Cycles available macros forward                                                                                     |
-| `cycle_prev`        | `<c-p>` | Cycles available macros backward                                                                                    |
-| `toggle_record`     | `q`     | Starts recording, press again to end recording                                                                      |
-| `yank_macro`        | `yq`    | Yank the currently selected macro, in human readable format into the default register                               |
-|                     |         |                                                                                                                     |
-| `:ToggleDelay `     |         | For complex macros over large counts, you can toggle a delay between macro playback using the `ToggleDelay` command |
-
 ### _Telescope_
 
 | Keymap              | Action                                                               |
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -19,50 +19,6 @@ return {
   },
 
   {
-    --
-    -- | Function            | Keymap  | Action                                                                                                              |
-    -- | ------------------- | ------- | ------------------------------------------------------------------------------------------------------------------- |
-    -- | `play_macro`        | `Q`     | Plays queued macro                                                                                                  |
-    -- | `stop_macro`        | `cq`    | Halts macro playback                                                                                                |
-    -- | `toggle_macro_menu` | `<m-q>` | Toggles popup macro menu                                                                                            |
-    -- | `cycle_next`        | `<c-n>` | Cycles available macros forward                                                                                     |
-    -- | `cycle_prev`        | `<c-p>` | Cycles available macros backward                                                                                    |
-    -- | `toggle_record`     | `q`     | Starts recording, press again to end recording                                                                      |
-    -- | `yank_macro`        | `yq`    | Yank the currently selected macro, in human readable format into the default register                               |
-    -- |                     |         |                                                                                                                     |
-    -- | `:ToggleDelay `     |         | For complex macros over large counts, you can toggle a delay between macro playback using the `ToggleDelay` command |
-    --
-    -- - _Telescope_
-    --   | Keymap | Action |
-    --   | ------------------- | -------------------------------------------------------------------- |
-    --   | `:Telescope macros` | Launch the Telescope extension using the `Telescope macros` command |
-    --   | `yq` | Yank the currently selected macro, in human readable format (normal) |
-    --   | `<cr>` | Queue the currently selected macro (insert, normal) |
-    --   | `<c-d>` | Delete the currently selected macro (insert) |
-    --   | `d` | Delete the currently selected macro |
-    "ecthelionvi/NeoComposer.nvim",
-    dependencies = {
-      "kkharji/sqlite.lua",
-    },
-    opts = {},
-    event = "VeryLazy",
-    config = function()
-      require("NeoComposer").setup({
-        keymaps = {
-          play_macro = "Q",
-          yank_macro = "yq",
-          stop_macro = "cq",
-          toggle_record = "<leader>q",
-          cycle_next = "<c-n>",
-          cycle_prev = "<c-p>",
-          toggle_macro_menu = "<m-q>",
-        },
-      })
-      require("telescope").load_extension("macros")
-    end,
-  },
-
-  {
     "Wansmer/treesj",
     keys = { "<space>m" },
     dependencies = { "nvim-treesitter/nvim-treesitter" },

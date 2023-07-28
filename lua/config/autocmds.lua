1f466be6f8934db266b78958ad2f58ad17f54512
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-27 23:08:41 -0300

    ➕ add: autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+--
+vim.api.nvim_create_user_command("DiffThis", ":Gitsigns diffthis<CR>", { desc = "Git Diff current buffer" })
+
+-- run on every file
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("setlocal formatoptions-=cro")
+  end,
+  desc = "Stop comment continuation on line below",
+})

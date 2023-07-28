23b4360e062c5001283006a22907f5f183c6e5c6
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-27 23:10:36 -0300

    🚀 feat: LazyVim setup

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
+-- global keumap function
+function Map(lhs, rhs, opts)
+  local mode = "n"
+  local options = { noremap = true, silent = true }
+  if opts then
+    if opts.mode then
+      mode = opts.mode
+      opts.mode = nil
+    end
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")

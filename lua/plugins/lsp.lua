ebd6dc369f02a7085b7c7bdb618520bd94c6e915
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-27 23:10:22 -0300

    ➕ add(lsp): zk + css + prisma + angular

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = function(_, opts)
+      opts.autoformat = true
+      opts.servers = vim.tbl_deep_extend("force", opts.servers, {
+        cssls = {},
+        prismals = {},
+        angularls = {},
+        emmet_ls = { filetypes = { "html", "gohtml" } },
+      })
+
+      opts.setup = {
+        zk = function()
+          require("zk").setup()
+          return true
+        end,
+      }
+    end,
+  },
+}

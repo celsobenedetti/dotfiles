1bf7003f3366cc6c7f1f86979463d22f4b1bd1b6
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-28 12:50:49 -0300

    🎨 style: cmp borders

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -17,5 +17,28 @@ return {
     config = function()
       require("nvim-surround").setup()
     end,
+    vscode = true,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "uga-rosa/cmp-dictionary" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "dictionary", keyword_length = 2 },
+      }))
+
+      ---@diagnostic disable-next-line: missing-fields
+      opts.matching = {
+        disallow_fuzzy_match = false,
+      }
+
+      opts.window = {
+        completion = cmp.config.window.bordered({ col_offset = -1 }),
+        documentation = cmp.config.window.bordered(),
+      }
+    end,
   },
 }

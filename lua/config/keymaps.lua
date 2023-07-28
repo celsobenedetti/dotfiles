4bfea2f210b22c996c6c0b3d3962d48962b3c086
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-27 21:59:14 -0300

    ➕ add: relative_jump function

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local track_position_before_relative_jump = require("functions.relative_jump")
+
+function Map(lhs, rhs, opts)
+  local mode = "n"
+  local options = { noremap = true, silent = true }
+  if opts then
+    if opts.mode then
+      mode = opts.mode
+      opts.mode = nil
+    end
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
+Map("k", track_position_before_relative_jump.up)
+Map("j", track_position_before_relative_jump.down)

2cecbe11c5e1ea547392eead030e188073854768
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-27 23:08:48 -0300

    ➕ add: options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.swapfile = false
+
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4 -- Size of an indent
+
+-- open buffers verticaly
+vim.cmd("autocmd FileType help wincmd L") --help
+vim.cmd("autocmd FileType man wincmd L") -- man

dd8bc513f5ad14d0cdef7a8f97f8648a21b8fce8
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-31 11:36:28 -0300

    ➕ add(harpoon): keymaps

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -35,23 +35,29 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
     config = function()
-      Map("<leader>'", require("harpoon.mark").add_file)
-      Map("''", require("harpoon.ui").toggle_quick_menu)
-      Map('"', "<C-^>")
+      local harpoon = require("harpoon.ui")
+      Map("<leader>m", require("harpoon.mark").add_file)
+      Map("mm", harpoon.toggle_quick_menu)
+      Map("<A-h>", harpoon.nav_prev, { desc = "Harpoon previous" })
+      Map("<A-l>", harpoon.nav_next, { desc = "Harpoon next" })
 
       local power_fingers = { "a", "s", "d", "f" }
       local power_fingers2 = { "h", "j", "k", "l" }
       for i = 1, 4 do
+        Map("<A-" .. tostring(i) .. ">", function()
+          harpoon.nav_file(i)
+        end)
+
         Map("'" .. tostring(i), function()
-          require("harpoon.ui").nav_file(i)
+          harpoon.nav_file(i)
         end)
 
         Map("'" .. power_fingers[i], function()
-          require("harpoon.ui").nav_file(i)
+          harpoon.nav_file(i)
         end)
 
         Map("'" .. power_fingers2[i], function()
-          require("harpoon.ui").nav_file(i)
+          harpoon.nav_file(i)
         end)
       end
     end,

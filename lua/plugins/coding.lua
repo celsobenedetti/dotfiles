c0c2a45404ba026a1520d6781c6851163db36a82
Celso Benedetti <celsobenedetti2@gmail.com>
2023-08-14 12:14:08 -0300

    🔧 fix: LuaSnip hotkeys only on needed modes

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,9 +4,9 @@ return {
   {
     "L3MON4D3/LuaSnip",
     keys = {
-      { "<C-l>", snippets.accept_or_go_next, mode = "i" },
-      { "<c-h>", snippets.go_back, mode = "i" },
-      { "<c-j>", snippets.toggle_option, mode = "i" },
+      { "<C-l>", snippets.accept_or_go_next, mode = { "i", "s" } },
+      { "<c-h>", snippets.go_back, mode = { "i", "s" } },
+      { "<c-j>", snippets.toggle_option, mode = { "i", "s" } },
     },
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load({

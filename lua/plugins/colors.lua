22a3e11490cec3ab1c938e9b1a1ad0ea29cd3a31
Celso Benedetti <celsobenedetti2@gmail.com>
2023-08-28 20:55:20 -0300

    🎨 style: github colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,13 +1,15 @@
 local colors = {
   github = "github_dark_high_contrast",
+  github_light = "github_light",
   tokyo = "tokyonight",
-  catppuccin = "catppuccin",
+  mocha = "catppuccin",
+  frappe = "catppuccin-frappe",
   tundra = "tundra",
   rose = "rose-pine",
   dracula = "dracula-soft",
 }
 
-local colorscheme = colors.dracula
+local colorscheme = colors.frappe
 
 local function is_enabled(color)
   return colorscheme == color
@@ -21,7 +23,7 @@ return {
     },
   },
 
-  { "projekt0n/github-nvim-theme", enabled = is_enabled(colors.github) },
+  { "projekt0n/github-nvim-theme", enabled = is_enabled(colors.github) or is_enabled(colors.github_light) },
   { "rose-pine/neovim", enabled = is_enabled(colors.rose) },
   { "sam4llis/nvim-tundra", enabled = is_enabled(colors.tundra) },
   { "Mofiqul/dracula.nvim", config = true, enabled = is_enabled(colors.dracula) },

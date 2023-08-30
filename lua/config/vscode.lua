c83597a963994fb98e8ba669fedaf92704c18eae
Celso Benedetti <celsobenedetti2@gmail.com>
2023-08-30 18:11:50 -0300

    🚀 feat(vscode): keymaps

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -1,11 +1,16 @@
-Map(
-  "<leader>e",
-  ":call VSCodeCall('workbench.action.toggleSidebarVisibility')<CR>",
-  { desc = "Toggle sidebar" }
-)
+Map("<leader>e", function()
+  vim.cmd("call VSCodeCall('workbench.action.toggleSidebarVisibility')")
+end, { desc = "Toggle sidebar" })
 
-Map(
-  "<leader>ff",
-  ":call VSCodeCall('workbench.action.quickOpen')<CR>",
-  { desc = "Quick Open pop-up (File search)" }
-)
+Map("<leader>ff", function()
+  vim.cmd("call VSCodeCall('workbench.action.quickOpen')")
+end, { desc = "Quick Open pop-up (File search)" })
+
+Map("<leader>bd", function()
+  vim.cmd("call VSCodeCall('workbench.action.files.save')")
+  vim.cmd("call VSCodeCall('workbench.action.closeActiveEditor')")
+end, { desc = "Close current editor (buffer)" })
+
+Map("<leader>cr", function()
+  vim.cmd("call VSCodeCall('editor.action.rename')")
+end, { desc = "Rename symbol" })

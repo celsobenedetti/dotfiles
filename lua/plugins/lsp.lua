c1aa2b0a0171519623693ec0db78cf72f01e77c9
Celso Benedetti <celsobenedetti2@gmail.com>
2023-09-01 15:58:17 -0300

    🚀 feat: sonarlint.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,7 +43,34 @@ return {
       vim.list_extend(opts.ensure_installed, {
         "shellcheck",
         "sqlfluff",
+        "sonarlint-language-server",
       })
     end,
+  },
+
+  {
+    "schrieveslaach/sonarlint.nvim",
+    url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+    opts = {
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          -- Ensure that sonarlint-language-server uses stdio channel
+          "-stdio",
+          "-analyzers",
+          -- paths to the analyzers you need, using those for python and java in this example
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+        },
+      },
+      filetypes = {
+        -- Tested and working
+        "python",
+        "cpp",
+        -- Requires nvim-jdtls, otherwise an error message will be printed
+        "java",
+      },
+    },
   },
 }

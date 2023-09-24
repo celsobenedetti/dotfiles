936089f3fa083b9a489e720135f53f84bc34c2e8
Celso Benedetti <celsobenedetti2@gmail.com>
2023-09-24 14:23:56 -0300

    🚀 feat: GPT.nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -42,6 +42,19 @@ return {
       })
     end,
   },
+  -- Lazy
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 
   -- copilot cmp source
   -- {

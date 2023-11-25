bac20a2530b7e9f1df8deb4aa66261f12461a762
Celso Benedetti <celsobenedetti2@gmail.com>
2023-11-25 18:55:39 -0300

    🔧 fix(sway): sway-select-window.sh

--- sway/.local/bin/sway-select-window.sh
+++ sway/.local/bin/sway-select-window.sh
@@ -3,8 +3,8 @@
 # Get available windows
 windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* .")
 
-# Select window with rofi
-selected=$(echo "$windows" | rofi -dmenu -i | awk '{print $1}')
+# Select window with wofi
+selected=$(echo "$windows" | wofi --dmenu -i | awk '{print $1}')
 
 # Tell sway to focus said window
 swaymsg [con_id="$selected"] focus

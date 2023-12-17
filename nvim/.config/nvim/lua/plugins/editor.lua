c5d0224796707ed0f517455d9909aceedca076d6
Celso Benedetti <celsobenedetti2@gmail.com>
2023-12-17 12:29:50 -0300

    add(vim): graphql treesitter parser

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -147,6 +147,7 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
         "css",
+        "graphql",
         "http",
         "hurl",
         "make",

da1325e40f130d78527e68ba667bf8ef00c2fd05
Celso Benedetti <celsobenedetti2@gmail.com>
2024-01-16 08:19:26 -0300

    feat(vim): telescope ignore file patterns

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -74,6 +74,7 @@ return {
           n = { ["<leader>dd"] = require("telescope.actions").delete_buffer },
           i = { ["<leader>dd"] = require("telescope.actions").delete_buffer },
         },
+        file_ignore_patterns = { "drupal", "python" },
       },
     },
     keys = {

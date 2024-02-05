3de82d44fe0bbe6b00365b6d02f241359b0f8cce
Celso Benedetti <celsobenedetti2@gmail.com>
2024-02-05 10:12:12 -0300

    fix(vim): work-week fold

--- nvim/.config/nvim/lua/functions/markdown/fold.lua
+++ nvim/.config/nvim/lua/functions/markdown/fold.lua
@@ -9,7 +9,6 @@ local week = {
 }
 
 --- If file is a work-week file, go to section for current week day and unfold it
-
 local function work_week()
   local file = vim.fn.expand("%:t")
   if type(file) ~= "string" then
@@ -19,19 +18,19 @@ local function work_week()
   local is_work_week = string.match(file, "work") and string.match(file, "week")
 
   if is_work_week then
+    -- close all folds
+    vim.api.nvim_feedkeys("zM", "n", true)
+    vim.api.nvim_feedkeys("za", "n", true)
+
     local day = vim.fn.system("date +%u") + 1
     if day > #week then
       day = #week
     end
-
-    -- close all folds
-    vim.api.nvim_feedkeys("zM", "n", true)
-
     -- search for the day of the week
     local searchWeekDay = "/" .. week[tonumber(day)] .. "<CR>"
     vim.api.nvim_feedkeys(Escape(searchWeekDay), "n", true)
 
-    -- unfold the day of the week
+    -- -- -- unfold the day of the week
     vim.api.nvim_feedkeys("VzO", "n", true)
   end
 end

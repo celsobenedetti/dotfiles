76686ed626f3dc6e16b7520a6094609e124ba88a
Celso Benedetti <celsobenedetti2@gmail.com>
2024-02-07 18:09:41 -0300

    feat(nvim): neotest-jest

--- nvim/.config/nvim/lua/plugins/test.lua
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -2,8 +2,24 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
-      "marilari88/neotest-vitest",
+      "nvim-treesitter/nvim-treesitter", -- this adding nvim-treesitter
+      {
+        "nvim-neotest/neotest-jest",
+        config = function()
+          require("neotest-jest")({
+            jestCommand = "pnpm test --",
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          })
+          -- code
+        end,
+      },
     },
-    opts = { adapters = { "neotest-vitest" } },
+    opts = function(_, opts)
+      vim.list_extend(opts.adapters, {
+        "neotest-jest",
+      })
+    end,
   },
 }

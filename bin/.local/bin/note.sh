3e6f7b2c9b518d786ba20d8437fbe32612f618f7
Celso Benedetti <celsobenedetti2@gmail.com>
2024-02-11 15:21:46 -0300

    feat(note.sh): "ok" create file as --no-edit

--- bin/.local/bin/note.sh
+++ bin/.local/bin/note.sh
@@ -8,6 +8,9 @@
 #
 # if second arugment is passed, create note in folder
 
+title=""
+destination="$NOTES/0-inbox/"
+
 template=$(
 	cat <<EOF
 
@@ -20,10 +23,8 @@ template=$(
 EOF
 )
 
-title=""
-
 if [[ -n "$1" ]]; then
-	title=$1
+	title="$1"
 else
 	temp='/tmp/note'
 	echo "$template" >"$temp"
@@ -37,10 +38,15 @@ if [[ -z $title ]]; then
 	exit 0
 fi
 
-destination="$NOTES/0-inbox/"
+if ! [[ "$2" == "ok" ]]; then
+	zk new -t "$title" "$destination" 2>/dev/null
+else
+	# create note without opening in editor
+	new_note=$(zk new -t "$title" -n 2>&1)
+	note_path=$(echo "$new_note" | head -n 1 | tr -d '\r')
+	note_file=$(basename "$note_path")
+	note_content=$(echo "$new_note" | tail -n +2)
 
-if [[ -n "$2" ]]; then
-	destination="$2"
+	note_path="$destination/$note_file"
+	echo "$note_content" >"$note_path"
 fi
-
-zk new -t "$title" "$destination" 2>/dev/null

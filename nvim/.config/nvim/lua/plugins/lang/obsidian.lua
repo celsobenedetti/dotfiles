5570ad405e635ff5ccb40a8155385c66d15b53c1
Celso Benedetti <celsobenedetti2@gmail.com>
2024-02-19 08:06:36 -0300

    fix(nvim): obsidian.lua

--- nvim/.config/nvim/lua/plugins/lang/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/lang/obsidian.lua
@@ -1,16 +1,7 @@
-local function random_letter_or_number()
-  local is_number = math.random(0, 1) == 1
-  if is_number then
-    return string.char(math.random(48, 57))
-  else
-    return string.char(math.random(65, 90))
-  end
-end
-
 return {
   {
     "epwalsh/obsidian.nvim",
-    commit = "06154ec6f2964632d53c8fea9f0c175f31357192",
+    -- commit = "06154ec6f2964632d53c8fea9f0c175f31357192",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
     ft = "markdown",
@@ -26,76 +17,59 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    config = function()
-      require("obsidian").setup({
-        new_notes_location = "notes_subdir",
-        workspaces = {
-          {
-            name = "notes",
-            path = "~/Documents/notes",
-            overrides = {
-              notes_subdir = "0-inbox",
-            },
+    opts = function(_, opts)
+      opts.new_notes_location = "notes_subdir"
+      opts.workspaces = {
+        {
+          name = "notes",
+          path = "~/Documents/notes",
+          overrides = {
+            notes_subdir = "0-inbox",
           },
         },
-        ui = {
-          checkboxes = {
-            -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-            ["~"] = { char = "x", hl_group = "ObsidianTilde" },
-            ["-"] = { char = "x", hl_group = "ObsidianTilde" },
-            -- Replace the above with this if you don't have a patched font:
-            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-            ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-
-            -- You can also add more custom ones...
-          },
-          reference_text = { hl_group = "ObsidianRefText" },
-          highlight_text = { hl_group = "ObsidianHighlightText" },
-          tags = { hl_group = "ObsidianTag" },
-          hl_groups = {
-            -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-            ObsidianTodo = { bold = true, fg = "#f78c6c" },
-            ObsidianDone = { bold = true, fg = "#89ddff" },
-            ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-            ObsidianTilde = { bold = true, fg = "#ff5370" },
-            ObsidianBullet = { bold = true, fg = "#89ddff" },
-            ObsidianRefText = { underline = true, fg = "#c792ea" },
-            ObsidianExtLinkIcon = { fg = "#c792ea" },
-            ObsidianTag = { italic = true, fg = "#89ddff" },
-            ObsidianHighlightText = { bg = "#75662e" },
-          },
+      }
+      opts.ui = {
+        enable = true, -- set to false to disable all additional syntax features
+        update_debounce = 200, -- update delay after a text change (in milliseconds)
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "x", hl_group = "ObsidianTilde" },
+          ["-"] = { char = "x", hl_group = "ObsidianTilde" },
+          ["x"] = { char = "✔", hl_group = "ObsidianDone" },
         },
-
-        note_id_func = function(title)
-          if title ~= nil then
-            title = title:gsub(" ", "-")
-            title = string.lower(title)
+        bullets = { char = "•", hl_group = "ObsidianBullet" },
+        external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = "ObsidianRefText" },
+        highlight_text = { hl_group = "ObsidianHighlightText" },
+        tags = { hl_group = "ObsidianTag" },
+        hl_groups = {
+          ObsidianTodo = { bold = true, fg = "#f78c6c" },
+          ObsidianDone = { bold = true, fg = "#89ddff" },
+          ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+          ObsidianTilde = { bold = true, fg = "#ff5370" },
+          ObsidianBullet = { bold = true, fg = "#89ddff" },
+          ObsidianRefText = { underline = true, fg = "#c792ea" },
+          ObsidianExtLinkIcon = { fg = "#c792ea" },
+          ObsidianTag = { italic = true, fg = "#89ddff" },
+          ObsidianHighlightText = { bg = "#75662e" },
+        },
+      }
+      opts.note_frontmatter_func = function(note)
+        local frontmatter = {
+          id = note.id,
+          aliases = note.aliases,
+          title = note.title,
+          date = note.date,
+          tags = note.tags,
+        }
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            frontmatter[k] = v
           end
-          local prefix = ""
-          for _ = 1, 4 do
-            prefix = prefix .. random_letter_or_number()
-          end
-
-          return prefix .. "_" .. title
-        end,
-        note_frontmatter_func = function(note)
-          local frontmatter = {
-            id = note.id,
-            aliases = note.aliases,
-            title = note.title,
-            date = note.date,
-            tags = note.tags,
-          }
-          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-            for k, v in pairs(note.metadata) do
-              frontmatter[k] = v
-            end
-          end
-          return frontmatter
-        end,
-      })
+        end
+        return frontmatter
+      end
     end,
   },
 }

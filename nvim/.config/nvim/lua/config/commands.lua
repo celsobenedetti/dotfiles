01b355d22ac0e48df212eb416c4619175e2d433c
Celso Benedetti <celsobenedetti2@gmail.com>
2024-02-21 23:55:57 -0300

    fix: path expansion on commands

--- nvim/.config/nvim/lua/config/commands.lua
+++ nvim/.config/nvim/lua/config/commands.lua
@@ -3,16 +3,16 @@ local tmux_popup = "!tmux popup -h 90\\% -w 80\\% "
 vim.api.nvim_create_user_command("Clip", "!echo % | xclip -sel clip", {})
 
 -- opens tmux popup with git diff
-vim.api.nvim_create_user_command("Diff", tmux_popup .. "git diff %", {})
+vim.api.nvim_create_user_command("Diff", tmux_popup .. "git diff %:p", {})
 
 -- prompts the user for log depth, then opens tmux popup with git log -p
 vim.api.nvim_create_user_command("Log", function()
   local depth = vim.fn.input("Git log depth: ", "", "file")
-  vim.cmd(tmux_popup .. "git log -p -n " .. depth .. " %")
+  vim.cmd(tmux_popup .. "git log -p -n " .. depth .. " %:p")
 end, {})
 
 -- opens current file in glow
-vim.api.nvim_create_user_command("Glow", tmux_popup .. "glow % -p", {})
+vim.api.nvim_create_user_command("Glow", tmux_popup .. "glow %:% -p", {})
 
 -- run bash on current line
 vim.api.nvim_create_user_command("Bash", ".!bash", {})

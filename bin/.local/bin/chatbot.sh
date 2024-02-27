78a784f7142e2418e1777dece83b2ada1573cacb
Celso Benedetti <celsobenedetti2@gmail.com>
2024-02-27 14:17:42 -0300

    feat: improve chatbot.sh

--- bin/.local/bin/chatbot.sh
+++ bin/.local/bin/chatbot.sh
@@ -2,27 +2,33 @@
 
 cd ~/work/chatbot/ || exit
 
-branch="$1"
+desc="Checkout, pull, build and boot up Chatbot:"
 
-if [ -z "$branch" ]; then
-	branch=$(
-		git branch \
-			-a \
-			--sort=-committerdate |
-			fzf \
-				--header "Chatbot: checkout and pull pull branch -> Run build scripts" \
-				--preview "git diff --color=always {1}" |
-			sed 's/remotes.origin.//' |
-			tr -d " "
-	)
-fi
+choose_branch() {
+	if [ -n "$1" ]; then
+		echo "$1"
+		return
+	fi
+
+	git branch \
+		-a \
+		--sort=-committerdate |
+		fzf \
+			--header "$desc" \
+			--preview "git diff --color=always {1}" |
+		sed 's/remotes.origin.//' |
+		tr -d " "
+
+}
+
+branch=$(choose_branch "$1")
 
 if [ -z "$branch" ]; then
 	echo "No branch selected"
 	exit
 fi
 
-# selected branch is different from the currently checked out
+# if selected branch is different from the currently checked out
 if ! echo "$branch" | grep -q -E "\*"; then
 	git switch "$branch"
 fi
